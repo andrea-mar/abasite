1980025574653acf9a4d124a3e7c94027df28493
andrea-mar <andreanistormar@gmail.com>
2021-07-06 13:57:11 +0100

    deleted sqlite3 db

--- abasite/settings.py
+++ abasite/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','abavblink.herokuapp.com']
 
 
 # Application definition
